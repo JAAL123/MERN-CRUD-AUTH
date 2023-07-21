9c99c6ef71d665b21b4eb405ffacbfefb78871d5
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-07-21 09:21:50 -0600

    consumo de api en cliente, añadido paquete de cors

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { registerRequest } from "../api/auth";
 
 export function RegisterPage() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  // const [values, setValues] = useState({
+  //   username: '',
+  //   email: '',
+  //   password: ''
+  // })
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md content-center">
-      <form action="" onSubmit={handleSubmit((values) => console.log(values))}>
+      <form
+        action=""
+        onSubmit={handleSubmit(async (values) => {
+          const res = await registerRequest(values);
+          console.log(res);
+          // setValues(values)
+          reset();
+        })}
+      >
         <input
           type="text"
           {...register("username", { required: true })}
@@ -23,7 +38,12 @@ export function RegisterPage() {
           className="w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md"
           placeholder="Contraseña"
         />
-        <button type="submit" className="border bg-zinc-700 border-inherit rounded p-2 hover:bg-zinc-600">Registrar</button>
+        <button
+          type="submit"
+          className="border bg-zinc-700 border-inherit rounded p-2 hover:bg-zinc-600"
+        >
+          Registrar
+        </button>
       </form>
     </div>
   );
--- src/app.js
+++ src/app.js
@@ -3,17 +3,20 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import tasksRoutes from "./routes/tasks.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/", (req,res) => {
-    res.send('api running')
-})
 
 app.use("/api", authRoutes);
 app.use("/api", tasksRoutes);
+app.use("/", (req, res) => {
+  res.send("api running");
+});
 
 export default app;
