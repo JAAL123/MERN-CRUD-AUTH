f541959b6b38f9d9fb4ccf384429f8a8dd1b03d9
TCAM\GIS0938 <alfredo.aguilar@movistar.com.sv>
2023-08-01 10:02:23 -0600

    cambio de estilos y añadido campo fecha

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,28 +1,52 @@
 import { UseTask } from "../context/TasksContex";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import utc from "dayjs/plugin/utc";
+import dayjs from "dayjs";
+
+dayjs.extend(utc);
 
 export function TaskCard({ task }) {
   const { deleteTask } = UseTask();
+
+  const navigate = useNavigate();
+
   return (
-    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md ">
-      <header className="flex justify-between">
-        <h1 className="text-2xl font-bold capitalize">{task.title}</h1>
-        <div className="flex gap-x-2 items-center">
+    <>
+      <div className="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <h5 className="mb-2 text-xl first-letter:capitalize  font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          {task.title}
+        </h5>
+        <p className="mb-4 text-base text-neutral-200 first-letter:capitalize">
+          {task.description}
+        </p>
+        <p className="text-slate-300 mb-4 text-base">
+          {dayjs(task.date).utc().format("DD/MM/YYYY")}
+        </p>
+        <div className="flex mt-auto border-t-2 border-neutral-600 px-6 py-3 text-left gap-3 ">
           <button
-            className="border bg-green-700 border-inherit rounded py-2 px-1 hover:bg-green-600"
+            type="button"
+            className="inline-block rounded bg-red-500 px-4 pb-2 pt-2.5 text-sm font-medium capitalize leading-normal text-white border hover:bg-red-600 "
+            data-te-ripple-init
+            data-te-ripple-color="light"
             onClick={() => {
               deleteTask(task._id);
             }}
           >
             Eliminar
           </button>
-          <Link className="hover:text-sky-600" to={`/task/${task._id}`}>Editar</Link>
+          <button
+            type="button"
+            className="inline-block rounded bg-yellow-500 px-4 pb-2 pt-2.5 text-sm font-medium capitalize leading-normal text-white border hover:bg-yellow-600 "
+            data-te-ripple-init
+            data-te-ripple-color="light"
+            onClick={() => {
+              navigate(`/task/${task._id}`);
+            }}
+          >
+            Editar
+          </button>
         </div>
-      </header>
-      <p className="text-slate-300">{task.description}</p>
-      <p className="text-slate-300">
-        {new Date(task.date).toLocaleDateString()}
-      </p>
-    </div>
+      </div>
+    </>
   );
 }
--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import { UseTask } from "../context/TasksContex";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect } from "react";
+import utc from "dayjs/plugin/utc";
+import dayjs from "dayjs";
+dayjs.extend(utc);
 
 export function TasksFormPage() {
   const {
@@ -24,16 +27,22 @@ export function TasksFormPage() {
         const task = await getTaskById(params.id);
         setValue("title", task.title);
         setValue("description", task.description);
+        setValue("date", dayjs.utc(task.date).format('YYYY-MM-DD'))
       }
     }
     loadTask();
   }, []);
 
   const onSubmit = handleSubmit((data) => {
+    const dataValid = {
+      ...data,
+      date: data.date ? dayjs.utc(data.date).format() : dayjs().utc().format(),
+    };
+
     if (params.id) {
-      updateTask(params.id, data);
+      updateTask(params.id, dataValid);
     } else {
-      createTask(data);
+      createTask(dataValid);
     }
     reset();
     navigate("/tasks");
@@ -51,6 +60,9 @@ export function TasksFormPage() {
         </h1>
 
         <form action="" onSubmit={onSubmit}>
+          <label htmlFor="title" className="text-base text-neutral-200">
+            Titulo:
+          </label>
           <input
             type="text"
             placeholder="Titulo"
@@ -61,25 +73,38 @@ export function TasksFormPage() {
           {errors.title && (
             <p className="text-red-600">El titulo es requerido</p>
           )}
+          <label htmlFor="description" className="text-base text-neutral-200">
+            Descripcion:
+          </label>
           <textarea
             rows="3"
             placeholder="Descripcion"
             {...register("description")}
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
           ></textarea>
-          <button
-            type="submit"
-            className="border bg-green-700 border-inherit rounded p-2 mx-2 hover:bg-green-600"
-          >
-            Guardar
-          </button>
-          <button
-            type="button"
-            className="border bg-red-700 border-inherit rounded p-2 mx-2 hover:bg-red-600"
-            onClick={onClick}
-          >
-            Cancelar
-          </button>
+          <label htmlFor="date" className="text-base text-neutral-200">
+            Fecha:
+          </label>
+          <input
+            type="date"
+            {...register("date")}
+            className="bg-zinc-700 text-white my-2 mx-2 rounded-md p-1"
+          />
+          <div className="flex mt-auto my-2 border-t-2 border-neutral-600 px-6 py-3 text-left gap-2 justify-start">
+            <button
+              type="submit"
+              className="border bg-green-700 border-inherit rounded p-2 mx-2 hover:bg-green-600"
+            >
+              Guardar
+            </button>
+            <button
+              type="button"
+              className="border bg-red-700 border-inherit rounded p-2 mx-2 hover:bg-red-600"
+              onClick={onClick}
+            >
+              Cancelar
+            </button>
+          </div>
         </form>
       </div>
     </div>
